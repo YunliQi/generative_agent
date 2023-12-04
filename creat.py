ce025a2de201a8883438a7d2dbd87af0ec6ed85e
WuJunde <izzy843794947@gmail.com>
2023-12-04 13:01:21 +0800

    cooperating into langchain pipline, mood: tired

--- creat.py
+++ creat.py
@@ -1,4 +1,19 @@
 import networkx as nx
+import math
+import faiss
+
+from langchain.docstore import InMemoryDocstore
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.retrievers import TimeWeightedVectorStoreRetriever
+from langchain.vectorstores import FAISS
+from datetime import datetime, timedelta
+from typing import List
+
+
+from langchain_experimental.generative_agents import (
+    GenerativeAgent,
+    GenerativeAgentMemory,
+)
 
 town_areas = ["Barthen's Provisions", "Lionshield Coster", "Stonehill Inn", "Phandalin Town Square"]
 town_areas = {"Phandalin Town Square": 'Town square of the town of Phandalin.',
@@ -29,15 +44,69 @@ town_people = {"Toblen Stonehill": "Toblen owns a trading post.",
 world_graph = nx.Graph()
 
 ### Response:'''
-def build_world():
+def creat_world(model):
 
     for town_area in town_areas.keys():
-    world_graph.add_node(town_area)
-    world_graph.add_edge(town_area, town_area)
+        world_graph.add_node(town_area)
+        world_graph.add_edge(town_area, town_area)
     for town_area in town_areas.keys():
-    world_graph.add_edge(town_area, "Phandalin Town Square")
+        world_graph.add_edge(town_area, "Phandalin Town Square")
     locations = {}
     for i in town_people.keys():
-    locations[i] = "Phandalin Town Square"
+        locations[i] = "Phandalin Town Square"
+        create_agent(model, name, age, traits, status)
 
     return locations
+
+
+def relevance_score_fn(score: float) -> float:
+    """Return a similarity score on a scale [0, 1]."""
+    # This will differ depending on a few things:
+    # - the distance / similarity metric used by the VectorStore
+    # - the scale of your embeddings (OpenAI's are unit norm. Many others are not!)
+    # This function converts the euclidean norm of normalized embeddings
+    # (0 is most similar, sqrt(2) most dissimilar)
+    # to a similarity function (0 to 1)
+    return 1.0 - score / math.sqrt(2)
+
+
+def create_new_memory_retriever():
+    """Create a new vector store retriever unique to the agent."""
+    # Define your embedding model
+    embeddings_model = HuggingFaceEmbeddings()
+    # Initialize the vectorstore as empty
+    embedding_size = 1536
+    index = faiss.IndexFlatL2(embedding_size)
+    vectorstore = FAISS(
+        embeddings_model.embed_query,
+        index,
+        InMemoryDocstore({}),
+        {},
+        relevance_score_fn=relevance_score_fn,
+    )
+    return TimeWeightedVectorStoreRetriever(
+        vectorstore=vectorstore, other_score_keys=["importance"], k=15
+    )
+
+def create_agent(model, name, age, traits, status):
+    tommies_memory = GenerativeAgentMemory(
+        llm=model,
+        memory_retriever=create_new_memory_retriever(),
+        verbose=False,
+        reflection_threshold=8,  # we will give this a relatively low number to show how reflection works
+    )
+
+    tommie = GenerativeAgent(
+        name=name,
+        age=age,
+        traits=traits,  # You can add more persistent traits here
+        status=status,  # When connected to a virtual world, we can have the characters update their status
+        memory_retriever=create_new_memory_retriever(),
+        llm=model,
+        memory=tommies_memory,
+    )
+
+    print("You are creating a new agent: " + str(name))
+    print(tommie.get_summary())
+
+    return tommie
