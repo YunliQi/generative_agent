0b208c1ac1f38fca4380e62b4453bdee39a62e37
WuJunde <izzy843794947@gmail.com>
2023-12-09 22:08:59 +0800

    update

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,3"
 os.environ['TRANSFORMERS_CACHE'] = '/mnt/iMVR/junde/.cache/huggingface/hub'
 
 from langchain.llms import HuggingFacePipeline
@@ -15,27 +15,33 @@ model = HuggingFacePipeline(pipeline=pipeline, model_kwargs={'temperature':0.7})
 
 locations, agents = creat_world(model)
 
+# init
+action_results = {}
+back_know = []
+for i in town_people.keys(): # generate bk knowledge for everyone
+  back_know.append(i+': '+ agents[i].get_summary(force_refresh=False))
+for i in town_people.keys(): # add to people mem
+  agents[i].memory.add_memory(back_know)
+  action_results[i] = i + 'is' + town_people[i]["status"]
+
 global_time = 0
 for repeats in range(5):
   global_time += 1
   print("In global time", global_time)
   action_prompts = {}
-  action_results = {}
+  people_description = []
   for location in town_areas.keys():
     people = []
     for i in town_people.keys():
       if locations[i] == location:
         people.append(i)
-    
+        people_description.append(action_results[i])
+
     # for name in people:
       # prompt = "You are {}. Your plans are: {}. You are currently in {} with the following description: {}. Your memories are: {}. It is currently {}:00. The following people are in this area: {}. You can interact with them.".format(name, plans[name], location, town_areas[location], '\n'.join(compressed_memories_all[name][-5:]), str(global_time), ', '.join(people))
-    people_description = []
-    for i in people: 
-      if i not in action_results: # initialize action results as the observations
-        action_results[i] = agents[i].get_summary(force_refresh=False)
-      people_description.append(i+': '+ action_results[i])
-
-
+    # for i in people: 
+    #   if i not in action_results: # initialize action results as the status, and add agent mem with description
+    #     action_results[i] = agents[i].get_summary(force_refresh=False)
     for i in people: # add observation to memory and react
       print("Mind Tree of people: ", i)
 
